48c3a2592b8e5c5275f9fe840598dcdd9e30c351
Dias Nurgaliyev <56669473+diz1l@users.noreply.github.com>
2024-12-06 18:01:46 +0100

    first kamasutra

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import style from './header.scss';
+
+const Header = () => {
+    return (
+        <header className='app-header'>
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Emblem_of_Kazakhstan_3d.svg/1200px-Emblem_of_Kazakhstan_3d.svg.png"
+                alt="Kazakhstan"/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/Components/Main/index.js
@@ -0,0 +1,1 @@
+export { default as Main_info } from './Main_info';
